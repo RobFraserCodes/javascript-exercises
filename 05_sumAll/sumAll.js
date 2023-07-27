f2c211e1da877805c80fccecfee7fa3ce72b3a32
Code Inverness <hello@rfraser.co.uk>
2023-07-27 18:42:39 +0100

    Completed Assignments
    All completed, all tests passed for 05, 06 and 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let sum = 0;
+    if (firstNum < 0 || secondNum < 0) {
+        return 'ERROR';
+    } else if (typeof firstNum !== 'number' || typeof secondNum !== 'number') {
+        return 'ERROR';
+    } else if (firstNum > secondNum) {
+        for (let i = secondNum; i <= firstNum; i++) {
+            sum += i;
+        }
+    } else if (firstNum < secondNum) {
+        for (let i = firstNum; i <= secondNum; i++) {
+            sum += i;
+        }
+    } else {
+        sum = firstNum + secondNum;
+    }
+    return sum;
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  let celcius = (tempInFahrenheit - 32) * (5 / 9);
+  celcius = Number(celcius.toFixed(1));
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelcius) {
+  let fahrenheit = tempInCelcius * (9 / 5) + 32;
+  fahrenheit = Number(fahrenheit.toFixed(1));
+  return fahrenheit;
 };
 
 // Do not edit below this line
