e1e8ae8f8db0c25f0c155866ef2f5b349ea126ce
Code Inverness <hello@rfraser.co.uk>
2023-07-28 12:53:27 +0100

    completed palindromes
    are learnt what palindromes are!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.toLowerCase().replace(/[.,?! ]/g, "");
+    let reverseString = newString.split("").reverse().join("");
+    // console.log(newString);
+    // console.log(reverseString);
+    if ( newString == reverseString ) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+palindromes("A car, a man, a maraca.");
 
 // Do not edit below this line
 module.exports = palindromes;
