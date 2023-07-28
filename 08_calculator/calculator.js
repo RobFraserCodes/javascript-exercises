7feadb31ed0cc23ad0b985ec5e8aae61a86412ae
Code Inverness <hello@rfraser.co.uk>
2023-07-28 12:30:29 +0100

    completed calculator
    all tests passed successfully

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,20 +6,23 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function(a, b) {
-	return a + b;
+//  This should sum up the numbers in the array
+const sum = function(a) {
+  const sum = a.reduce((a, b) => a + b, 0);
+  return sum;
 };
 
-const multiply = function(a, b) {
-  return a * b;
+const multiply = function(...args) {
+  return args.reduce((a, b) => a * b, 1);
 };
 
 const power = function(a, b) {
   return a ** b;
 };
 
-const factorial = function(a, b) {
-  return a * b;
+// The factorial function (symbol: !) says to multiply all whole numbers from our chosen number down to 1.
+const factorial = function(n) {
+  return n < 2 ? 1 : n * factorial(n - 1);
 };
 
 // Do not edit below this line
