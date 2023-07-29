2df1e1c6fd596a62e1c71eaa0865a7d717b7b44e
Code Inverness <hello@rfraser.co.uk>
2023-07-29 13:02:34 +0100

    completed part 5
    finished the last few excercises:
    - 10 fibonacci
    - 11 getTheTitles
    - 12 findTheOldest

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n<0) {
+        return "OOPS";
+    }
+    let fibonacciSeq = [0,1];
 
+    for(let i = 2; i <= n; i++){
+        fibonacciSeq[i] = fibonacciSeq[i-1] + fibonacciSeq[i-2];
+    }
+
+    return fibonacciSeq[n];
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const getAge = function(birth, death) {
+        if (!death) {
+            death = new Date().getFullYear();
+        }
+        return death - birth;
+    };
 
+    return people.reduce((oldest, person) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+        const personAge = getAge(person.yearOfBirth, person.yearOfDeath)
+        return oldestAge < personAge ? person : oldest
+    })
 };
 
 // Do not edit below this line
